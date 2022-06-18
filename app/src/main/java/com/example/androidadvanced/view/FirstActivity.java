c76a1e88c67ba306d53eefdb7fe548c4ee16c43d
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-06-18 14:05:11 +0500

    new

--- app/src/main/java/com/example/androidadvanced/view/FirstActivity.java
+++ app/src/main/java/com/example/androidadvanced/view/FirstActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androidadvanced.R;
 
 import java.util.Locale;
+
+import timber.log.Timber;
 
 public class FirstActivity extends AppCompatActivity {
     private Button btn_jp;
@@ -18,6 +20,8 @@ public class FirstActivity extends AppCompatActivity {
     private Button btn_ru;
     private Button btn_uz;
     private Button btn_cn;
+    private Button btn_check_first;
+    private Button btn_check_second;
     private TextView tv_desc;
 
     @Override
@@ -25,17 +29,25 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
+//        Timber.plant(new Timber.DebugTree());
+        Timber.i("Timber logging is ready");
+        btn_check_first = findViewById(R.id.btn_check_first);
+        btn_check_second = findViewById(R.id.btn_check_second);
+
         Button btn_en = findViewById(R.id.btn_en);
         btn_en.setOnClickListener(view -> {
             setLocale("en");
+            Timber.i("click english ");
         });
         btn_jp = findViewById(R.id.btn_jp);
         btn_jp.setOnClickListener(view -> {
             setLocale("ja");
+            Timber.e("Error Message");
         });
         btn_kr = findViewById(R.id.btn_kr);
         btn_kr.setOnClickListener(view -> {
             setLocale("ko");
+            Timber.d("Debug Message");
         });
         btn_ru = findViewById(R.id.btn_ru);
         btn_ru.setOnClickListener(view -> {
@@ -50,6 +62,18 @@ public class FirstActivity extends AppCompatActivity {
             setLocale("zh");
         });
         tv_desc = findViewById(R.id.tv_desc);
+
+        btn_check_first.setOnClickListener(view -> {
+            morphDoneAndRevert();
+        });
+        btn_check_second.setOnClickListener(view -> {
+
+            Toast.makeText(this, "Yahooo", Toast.LENGTH_SHORT).show();
+
+        });
+    }
+
+    void morphDoneAndRevert() {
     }
 
     private void setLocale(String lan) {
